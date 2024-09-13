770aada6b46a8238803c8f0648b67d9f6ff801d2
Markus Füreder <markus.fuereder@car-cutter.com>
2024-09-13 23:34:09 +0200

    feat: :sparkles: Implement rough pull script.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dotfiles
+
+## locally
+
+```sh
+/bin/bash -c "$(curl -fsSL http://127.0.0.1:5500/pull.sh)"
+```
+
+## remote
+
+```sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/markus-dot-fuereder/dotfiles/main/pull.sh)"
+```
---
+++ pull.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+DRY="NO"
+DRY="YES"
+
+if [ $DRY == "YES" ]; then
+    echo "DRY RUN ..."
+fi
+
+CURRENT_DIR=$PWD
+INSTALL_DIR="/etc"
+TMP_INSTALL_DIR="/tmp"
+GITHUB_USER="markus-dot-fuereder"
+REPO="dotfiles"
+REPO_DIR="$INSTALL_DIR"/"$REPO"
+REPO_TMP_DIR="$TMP_INSTALL_DIR"/"$REPO".tmp
+
+set -u
+
+clean_exit() {
+  if [ -d "$REPO_TMP_DIR" ]; then
+    rm -rf $REPO_TMP_DIR
+  fi
+  cd $CURRENT_DIR
+  exit $1
+}
+
+abort() {
+  printf "%s\n" "$@" >&2
+  clean_exit 1
+}
+
+# CLONE REPO IF NOT EXISTS
+REPO_URI="git@github.com:$GITHUB_USER/$REPO.git"
+if [ ! -d "$REPO_DIR" ]; then
+    echo "Cloning $REPO_URI to $REPO_TMP_DIR"
+    git clone $REPO_URI $REPO_TMP_DIR || abort "Failed to clone $REPO_URI"
+    sudo mv $REPO_TMP_DIR $REPO_DIR
+    # echo "Cloning $REPO_URI to $REPO_DIR"
+    # sudo git clone $REPO_URI $REPO_DIR || abort "Failed to clone $REPO_URI"
+else
+    git -C $REPO_DIR pull || abort "Failed to pull $REPO_URI"
+fi
+
+# MOVE TO TMP DIR
+cd $REPO_DIR
+ls -la
+CONFIG_DIR=~/.config
+mkdir -p CONFIG_DIR
+
+COLLECTION="collection"
+COLLECTION_PATH="$REPO_DIR"/"$COLLECTION"/*
+for SRC in $COLLECTION_PATH
+do 
+    [ -L "${SRC%/}" ] && continue
+    [ "$SRC" = "$COLLECTION_PATH" ] && continue
+
+    name=$(basename "$SRC")
+    echo "$SRC $name"
+    DST="$CONFIG_DIR"/"$name"
+    if [ $DEBUG != "YES" ]; then
+        echo "ln -s $SRC ~/$TARGET"
+    else
+        # ln -s $SRC ~/$TARGET
+        echo "ln -s $SRC ~/$TARGET"
+    fi
+done
+
+clean_exit 0
+
+
+
+
+
+
+
+
+
+# for file in "$COLLECTION/**"; do
+
+#     [ -L "${file%/}" ] && continue
+#     echo "$file"
+#     # if [[ -d "$file" ]]; then
+#     #   TARGET=".config/$file"
+#     #   echo "$file - $COLLECTION/$TARGET ~/$TARGET"
+#     # fi
+# done
