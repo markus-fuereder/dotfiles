ec50f7cb3351483a395fa6fd20d5511ba23c1fda
Markus Füreder <markus.fuereder@car-cutter.com>
2025-04-07 14:24:33 +0200

    refactor: ♻️ Update setup.

--- collection/nix/config.nix
+++ collection/nix/config.nix
@@ -1,15 +1,14 @@
 { pkgs, config, ... }:
 {
-    # SYSTEM =======================================================================================
-    # The `system.stateVersion` option is not defined in your
-    # nix-darwin configuration. The value is used to conditionalize
-    # backwards‐incompatible changes in default settings. You should
-    # usually set this once when installing nix-darwin on a new system
-    # and then never change it (at least without reading all the relevant
-    # entries in the changelog using `darwin-rebuild changelog`).
-    system.stateVersion = 6; # Do not change this
+    # SYSTEM ===========================================================================================================
+    # The `system.stateVersion` option is not defined in your nix-darwin configuration. The value is used to
+    # conditionalize backwards‐incompatible changes in default settings. You should usually set this once when
+    # installing nix-darwin on a new system and then never change it (at least without reading all the relevant entries
+    # in the changelog using `darwin-rebuild changelog`).
+    # So, do not change this value
+    system.stateVersion = 6;
 
-    # NIX ==========================================================================================
+    # NIX ==============================================================================================================
     nix = {
         settings.experimental-features = "nix-command flakes";
         optimise.automatic = true;
@@ -19,13 +18,13 @@
         };
     };
 
-    # NIX PKGS =====================================================================================
+    # NIX PKGS =========================================================================================================
     nixpkgs = {
         config.allowUnfree = true;
         hostPlatform = "aarch64-darwin";
     };
 
-    # ZSH ==========================================================================================
+    # ZSH ==============================================================================================================
     programs.zsh = {
         enable = true;
         enableCompletion = true;
@@ -33,65 +32,97 @@
         enableFzfCompletion = true;
         enableFzfGit = true;
         enableFzfHistory = true;
-        promptInit = ''
+        # Initialization script for zsh --------------------------------------------------------------------------------
+        shellInit = ''
+            # Powerlevel10k ............................................................................................
             source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
             source /etc/dotfiles/collection/powerlevel10k/p10k.zsh
+            if [[ -r "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+                source "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+            fi
 
-            # if [[ -r "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-            #     source "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-            # fi
+            # Autosuggestions ..........................................................................................
+            source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+            # Syntax-Highlighting ......................................................................................
+            source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+            # fnm / nvm autoload .......................................................................................
+            eval "$(fnm env --use-on-cd --shell zsh)"
+
+            # ZSH Config, flags, styles & environment variables ........................................................
+            source /etc/dotfiles/collection/zsh/config.zsh
+
+            # Environment ..............................................................................................
+            source /etc/dotfiles/collection/zsh/env.zsh
         '';
     };
 
-    # PACKAGES =====================================================================================
+    # GIT ==============================================================================================================
+    # programs.git = {
+    #     enable = true;
+    #     userName = "Markus Füreder";
+    #     userEmail = "markus.fuereder@gmail.com";
+    #     ignores = [ 
+    #         ".DS_Store"
+    #         ".next"
+    #         ".node_modules"
+    #     ];
+    #     extraConfig = {
+    #         init.defaultBranch = "main";
+    #     };
+    # };
+
+    # PACKAGES =========================================================================================================
     environment.systemPackages = with pkgs; [
-        # Fonts ------------------------------------------------------------------------------------
-        meslo-lgs-nf # ................................................. Nerd Font for Powerlevel10k
-        fira-code # ............................................................... Programming font
+        # Fonts --------------------------------------------------------------------------------------------------------
+        meslo-lgs-nf # ..................................................................... Nerd Font for Powerlevel10k
+        fira-code # ................................................................................... Programming font
 
-        # Terminal emulator and shell --------------------------------------------------------------
-        oh-my-zsh # .................................................................. zsh framework
-        zsh-powerlevel10k # ............................................ Powerlevel10k theme for zsh
-        kitty
-        # zsh-autosuggestions
-        # zsh-syntax-highlighting
+        # Shell & Terminal ---------------------------------------------------------------------------------------------
+        oh-my-zsh # .................................................................................. Framework for zsh
+        zsh-powerlevel10k # ................................................................ Theme powerlevel10k for zsh
+        zsh-autosuggestions # ........................................................... Plugin autosuggestions for zsh
+        zsh-syntax-highlighting # ................................................... Plugin syntax highlighting for zsh
+        kitty # ...................................................................................... Terminal emulator
+        tmux # .................................................................................... Terminal multiplexer
 
-        # CLI Tools --------------------------------------------------------------------------------
-        neofetch # ...................................................... Display system information
-        vim # .......................................................................... Text editor
-        git # ...................................................................... Version control
-        lazygit # .............................................................. Terminal UI for git
-        mkalias # ................................................................... Create aliases
-        bat # ................................................................ Replacement for `cat`
-        lsd # ................................................................. Replacement for `ls`
-        # eza # ............................................................... Replacement for `ls`
-        zoxide # .............................................................. Replacement for `cd`
-        fzf # .......................................................................... Fuzzy finder
-        pay-respects # ............................. Correct previous console command like "thefuck"
-        ripgrep # ........................................................... Search files with regex
-        imagemagick # ...................................................... Image manipulation tool
-        mas # ................................................. Mac App Store command line interface
+        # CLI Tools ----------------------------------------------------------------------------------------------------
+        neofetch # .......................................................................... Display system information
+        vim # .............................................................................................. Text editor
+        git # .......................................................................................... Version control
+        lazygit # .................................................................................. Terminal UI for git
+        mkalias # ....................................................................................... Create aliases
+        bat # .................................................................................... Replacement for `cat`
+        lsd # ..................................................................................... Replacement for `ls`
+        # eza # ................................................................................... Replacement for `ls`
+        zoxide # .................................................................................. Replacement for `cd`
+        fzf # ............................................................................................. Fuzzy finder
+        pay-respects # ................................................. Correct previous console command like "thefuck"
+        ripgrep # .............................................................................. Search files with regex
+        imagemagick # .......................................................................... Image manipulation tool
+        mas # ..................................................................... Mac App Store command line interface
 
-        # Development Tools ------------------------------------------------------------------------
-        fnm # ......................................................... fast Node.js version manager
-        # vscode # .............................................................. Visual Studio Code
+        # Development Tools --------------------------------------------------------------------------------------------
+        fnm # ............................................................................. fast Node.js version manager
+        # vscode # .................................................................................. Visual Studio Code
         vscode-with-extensions
-        # vscode # ........................................... Visual Studio Code without extensions
-        # android-studio # ...................................................... Android Studio IDE
+        # vscode # ............................................................... Visual Studio Code without extensions
+        # android-studio # .......................................................................... Android Studio IDE
 
-        # Languages --------------------------------------------------------------------------------
-        ruby # ........................................................... Ruby programming language
+        # Languages ----------------------------------------------------------------------------------------------------
+        ruby # ............................................................................... Ruby programming language
 
-        # Apps -------------------------------------------------------------------------------------
-        # _1password-gui # .......................................................... Password manager
-        # rectangle-pro # ................................................ Window management for macOS
-        obsidian # ............................................ Note-taking and knowledge management
-        firefox # ....................................................................... Web browser
-        google-chrome # ................................................................ Web browser
+        # Apps ---------------------------------------------------------------------------------------------------------
+        # _1password-gui # ............................................................................ Password manager
+        # rectangle-pro # .................................................................. Window management for macOS
+        obsidian # ................................................................ Note-taking and knowledge management
+        firefox # .......................................................................................... Web browser
+        google-chrome # .................................................................................... Web browser
 
     ];
 
-    # ALIASES ======================================================================================
+    # ALIASES ==========================================================================================================
     environment.shellAliases = {
         nix-count-garbage = "nix-store --gc --print-dead | wc -l";
         nix-rebuild = "darwin-rebuild switch --flake \"$(readlink -f ~/.config/nix)#shared\"";
@@ -104,9 +135,11 @@
         grep = "rg";
         nvm = "fnm";
         lg = "lazygit";
+        config_link="sh /etc/dotfiles/link.sh";
+        config_pull="sh /etc/dotfiles/pull.sh";
     };
 
-    # HOMEBREW =====================================================================================
+    # HOMEBREW =========================================================================================================
     # homebrew = {
     #   # Homebrew needs to be installed on its own!
     #   enable = true;
