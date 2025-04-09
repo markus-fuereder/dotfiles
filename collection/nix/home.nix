4116d70720a6248324a3e7fd93401fcdd82346f2
Markus Füreder <markus.fuereder@car-cutter.com>
2025-04-09 09:44:34 +0200

    refactor: :recycle: Update setup.

--- collection/nix/home.nix
+++ collection/nix/home.nix
@@ -10,7 +10,13 @@
     ];
   };
 
+  # ZSH ================================================================================================================
+  #! Keep this, otherwise home-manager will not create the ~/.zshrc file and programs like vscode will not source it.
+  programs.zsh = {
+    enable = true;
+  };
 
+  # VSCode =============================================================================================================
   programs.vscode = {
     enable = true;
   };
