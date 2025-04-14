44a50b86fd570ff7213d25ef074b45f727106a06
Markus Füreder <markus.fuereder@car-cutter.com>
2025-04-14 09:15:46 +0200

    refactor: :recycle: Update setup.

--- collection/zsh/env.zsh
+++ collection/zsh/env.zsh
@@ -3,6 +3,7 @@
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 export BIN=/run/current-system/sw/bin/
+export BREW_HOME=/opt/homebrew
 
 # FVM - Flutter Version Management =====================================================================================
 export FVM_CACHE_PATH=$HOME/.fvm/versions
@@ -10,6 +11,10 @@ export FVM_CACHE_PATH=$HOME/.fvm/versions
 # JAVA =================================================================================================================
 export JAVA_BIN="$(readlink -f $BIN/java)"
 export JAVA_HOME=${JAVA_BIN:h:h}
+# export OPEN_JDK=$BREW_HOME/opt/openjdk@17
+# export JDK_HOME=$OPEN_JDK/bin
+# export JAVA_HOME=$OPEN_JDK/libexec/openjdk.jdk/Contents/Home
+# export PATH=$JDK_HOME:$JAVA_HOME:$PATH
 
 # ANDROID ==============================================================================================================
 export ANDROID_HOME=$HOME/Library/Android/sdk
