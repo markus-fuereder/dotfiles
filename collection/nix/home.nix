b5bf939fe1263df08b1e1ffbf12bf88fdd0253c9
Markus Füreder <markus.fuereder@gmail.com>
2026-06-24 17:58:34 +0200

    refactor: Improve docker

--- collection/nix/home.nix
+++ collection/nix/home.nix
@@ -15,8 +15,14 @@
     ];
     file.".local/bin/claude".source = "${pkgs.claude-code}/bin/claude";
 
+    # set colima as docker context
+    activation.colima = lib.hm.dag.entryAfter ["writeBoundary" ] ''
+      export PATH="${lib.makeBinPath [ pkgs.docker pkgs.colima ]}:/usr/bin:$PATH"
+      $DRY_RUN_CMD ${pkgs.colima}/bin/colima start --vm-type vz
+      $DRY_RUN_CMD ${pkgs.docker}/bin/docker context use colima
+    '';
     # uv-managed global CLI tools
-    activation.uvTools = lib.hm.dag.entryAfter ["writeBoundary" ] ''
+    activation.uv = lib.hm.dag.entryAfter ["writeBoundary" ] ''
       export PATH="/Users/markus/.local/bin:$PATH"
       $DRY_RUN_CMD ${pkgs.uv}/bin/uv tool install 'graphifyy'
       $DRY_RUN_CMD ${pkgs.uv}/bin/uv tool install 'headroom-ai[mcp]'
