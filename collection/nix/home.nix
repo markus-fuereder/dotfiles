97d222b992ac629fad69060dbaf10e19f1fb2a7e
Markus Füreder <markus.fuereder@gmail.com>
2026-06-30 11:37:24 +0200

    refactor: Update graphify

--- collection/nix/home.nix
+++ collection/nix/home.nix
@@ -16,16 +16,17 @@
     file.".local/bin/claude".source = "${pkgs.claude-code}/bin/claude";
 
     # set colima as docker context
-    activation.colima = lib.hm.dag.entryAfter ["writeBoundary" ] ''
+    activation.colima = lib.hm.dag.entryAfter ["writeBoundary"] ''
       export PATH="${lib.makeBinPath [ pkgs.docker pkgs.colima ]}:/usr/bin:$PATH"
       $DRY_RUN_CMD ${pkgs.colima}/bin/colima start --vm-type vz
       $DRY_RUN_CMD ${pkgs.docker}/bin/docker context use colima
     '';
     # uv-managed global CLI tools
-    activation.uv = lib.hm.dag.entryAfter ["writeBoundary" ] ''
+    activation.uv = lib.hm.dag.entryAfter ["writeBoundary"] ''
       export PATH="/Users/markus/.local/bin:$PATH"
-      $DRY_RUN_CMD ${pkgs.uv}/bin/uv tool install 'graphifyy'
+      $DRY_RUN_CMD ${pkgs.uv}/bin/uv tool install 'graphifyy[gemini]' --force
       $DRY_RUN_CMD ${pkgs.uv}/bin/uv tool install 'headroom-ai[proxy,mcp]'
+      $DRY_RUN_CMD ${pkgs.uv}/bin/uv tool install 'markitdown[pdf, docx, pptx, xlsx, xls]'
     '';
   };
 
